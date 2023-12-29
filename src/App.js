7ab7e0dd7f43e3a967b59eda171dfe83a3b5d4c0
Shiza Batool <91551269+Shiza-Batool@users.noreply.github.com>
2023-12-30 01:29:46 +0500

    Work Section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from "./Components/Home"
 import About from "./Components/About"
 import Contact from "./Components/Contact"
 import Footer from "./Components/Footer"
+import Work from "./Components/Work"
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Work />
       <Contact />
       <Footer />
     </div>
