9c7f8c09e983e5b46a4baa222ed409ea6729fc0c
Shiza Batool <91551269+Shiza-Batool@users.noreply.github.com>
2023-12-30 01:26:07 +0500

    Footer Section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./Components/Home"
 import About from "./Components/About"
 import Contact from "./Components/Contact"
+import Footer from "./Components/Footer"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <About />
       <Contact />
+      <Footer />
     </div>
   );
 }
